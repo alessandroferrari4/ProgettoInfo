843bc72df889d57bad8f775db7b19eaa456015f8
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-24 16:03:19 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,42 +1,80 @@
 <?php
-session_start();
-require_once('dbconfig.php');
+   include("dbconfig.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM users WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+    <html>
 
-if (isset($_SESSION['session_id'])) {
-    header('Location: dashboard.php');
-    exit;
-}
+    <head>
+        <title>Login Page</title>
 
-if (isset($_POST['login'])) {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
-    
-    if (empty($username) || empty($password)) {
-        $msg = 'Inserisci username e password %s';
-    } else {
-        $query = "
-            SELECT username, password
-            FROM users
-            WHERE username = :username
-        ";
-        
-        $check = $pdo->prepare($query);
-        $check->bindParam(':username', $username, PDO::PARAM_STR);
-        $check->execute();
-        
-        $user = $check->fetch(PDO::FETCH_ASSOC);
-        
-        if (!$user || password_verify($password, $user['password']) === false) {
-            $msg = 'Credenziali utente errate %s';
-        } else {
-            session_regenerate_id();
-            $_SESSION['session_id'] = session_id();
-            $_SESSION['session_user'] = $user['username'];
+        <style type="text/css">
+            body {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 14px;
+            }
             
-            header('Location: insert.html');
-            exit;
-        }
-    }
-    
-    printf($msg, '<a href="../login.html">torna indietro</a>');
-}?>
+            label {
+                font-weight: bold;
+                width: 100px;
+                font-size: 14px;
+            }
+            
+            .box {
+                border: #666666 solid 1px;
+            }
+        </style>
+
+    </head>
+
+    <body bgcolor="#FFFFFF">
+
+        <div align="center">
+            <div style="width:300px; border: solid 1px #333333; " align="left">
+                <div style="background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+
+                <div style="margin:30px">
+
+                    <form action="" method="post">
+                        <label>UserName  :</label><input type="text" name="username" class="box" /><br /><br />
+                        <label>Password  :</label><input type="password" name="password" class="box" /><br/><br />
+                        <input type="submit" value=" Submit " /><br />
+                    </form>
+
+                    <div style="font-size:11px; color:#cc0000; margin-top:10px">
+                        <?php echo $error; ?>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+
+    </html>
