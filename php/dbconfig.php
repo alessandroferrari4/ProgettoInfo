d15554d3835e1ada84fff971631045e2e918b137
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-25 22:41:54 +0100

    Update dbconfig.php

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,7 +1,10 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', '');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'school');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $servername = "localhost";
+   $username = "root";
+   $password = "root";
+   $dbname = "school";
+   $db = new mysqli($servername, $username, $password, $dbname);
+   if ($db->connect_error) {
+     die("Connection failed: " . $db->connect_error);
+   }
 ?>
