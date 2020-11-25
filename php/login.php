08b57b94a44fc71c0191b382d9f680d2b9029566
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-25 22:41:48 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,80 +1,70 @@
 <?php
-   include("dbconfig.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM users WHERE username = '$myusername' and passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
+include_once('dbconfig.php');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $myusername = mysqli_real_escape_string($db, $_POST['username']);;
+    $mypassword = mysqli_real_escape_string($db, $_POST['password']);;
+
+    $sql = "SELECT id FROM users WHERE username = '$myusername' AND passcode = '$mypassword'";
+    $result = $db->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo $row['username'];
+        header("location: welcome.php");
+        exit();
+    } else {
+        $error = 'Username or password invalid';
+    }
+}
 ?>
-    <html>
+<html>
 
-    <head>
-        <title>Login Page</title>
+<head>
+    <title>Login Page</title>
 
-        <style type="text/css">
-            body {
-                font-family: Arial, Helvetica, sans-serif;
-                font-size: 14px;
-            }
-            
-            label {
-                font-weight: bold;
-                width: 100px;
-                font-size: 14px;
-            }
-            
-            .box {
-                border: #666666 solid 1px;
-            }
-        </style>
+    <style type="text/css">
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 14px;
+        }
 
-    </head>
+        label {
+            font-weight: bold;
+            width: 100px;
+            font-size: 14px;
+        }
 
-    <body bgcolor="#FFFFFF">
+        .box {
+            border: #666666 solid 1px;
+        }
+    </style>
 
-        <div align="center">
-            <div style="width:300px; border: solid 1px #333333; " align="left">
-                <div style="background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+</head>
 
-                <div style="margin:30px">
+<body bgcolor="#FFFFFF">
 
-                    <form action="" method="post">
-                        <label>UserName  :</label><input type="text" name="username" class="box" /><br /><br />
-                        <label>Password  :</label><input type="password" name="password" class="box" /><br/><br />
-                        <input type="submit" value=" Submit " /><br />
-                    </form>
+    <div align="center">
+        <div style="width:300px; border: solid 1px #333333; " align="left">
+            <div style="background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
 
-                    <div style="font-size:11px; color:#cc0000; margin-top:10px">
-                        <?php echo $error; ?>
-                    </div>
+            <div style="margin:30px">
 
+                <form action="" method="post">
+                    <label>UserName :</label><input type="text" name="username" class="box" /><br /><br />
+                    <label>Password :</label><input type="password" name="password" class="box" /><br /><br />
+                    <input type="submit" value=" Submit " /><br />
+                </form>
+
+                <div style="font-size:11px; color:#cc0000; margin-top:10px">
+                    <?php echo $error; ?>
                 </div>
 
             </div>
 
         </div>
 
-    </body>
+    </div>
 
-    </html>
+</body>
+
+</html>
