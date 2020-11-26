fbb011aead0727ee1753db8a5a13e8b211fee60c
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-26 12:49:10 +0100

    Update login.php

--- public/login.php
+++ public/login.php
@@ -14,10 +14,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 exit();
         }
         else{
-            $error="password sbagliata";
+            $error="Password sbagliata";
         }
     }else{
-        $error="username o password sbagliati";
+        $error="Username o password sbagliati";
         }
     }
 
@@ -25,42 +25,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
+
     <title>Login Page</title>
-
-    <style type="text/css">
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 14px;
-        }
-
-        label {
-            font-weight: bold;
-            width: 100px;
-            font-size: 14px;
-        }
-
-        .box {
-            border: #666666 solid 1px;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/data.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
 
 </head>
 
-<body bgcolor="#FFFFFF">
+<body>
 
-    <div align="center">
-        <div style="width:300px; border: solid 1px #333333; " align="left">
-            <div style="background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+    <div class="form">
+        <div class="a-container">
+            <div class="b-container"><b>Login</b></div>
 
             <div style="margin:30px">
 
                 <form action="" method="post">
-                    <label>UserName :</label><input type="text" name="username" class="box" /><br /><br />
-                    <label>Password :</label><input type="password" name="password" class="box" /><br /><br />
+                    <label>UserName:</label><input type="text" name="username" class="box" /><br /><br />
+                    <label>Password:</label><input type="password" name="password" class="box" /><br /><br />
                     <input type="submit" value=" Submit " /><br />
                 </form>
 
-                <div style="font-size:11px; color:#cc0000; margin-top:10px">
+                <div class="error">
                     <?php echo $error; ?>
                 </div>
 
