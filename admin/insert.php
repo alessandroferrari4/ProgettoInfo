2ed6883ed5a2f8d28a35b0290de760cece679898
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-26 21:34:32 +0100

    Update insert.php
    Controllo funzionante nel caso si inserisca uno studente già inserito, controllando il codice fiscale (SSN)

--- admin/insert.php
+++ admin/insert.php
@@ -1,56 +1,73 @@
 <?php
 include_once('dbconfig.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['namee'];
-    $surname = $_POST['surname'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
     $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $ssn = $_POST['ssn'];
     $class = $_POST['class'];
+    $section = $_POST['section'];
     $classroom = $_POST['classroom'];
-    $subject = $_POST['subject'];
+    $specialization = $_POST['specialization'];
 
     $newdate = date('Y-m-d', strtotime($dob));
 
-    $sql = "INSERT INTO students (namee,surname,dob,class,classroom,subjectt)
-     VALUES ('$name','$surname','$newdate','$class','$classroom','$subject')";
-    $sql1 = "SELECT * FROM students";
+    $sql = "INSERT INTO students (firstname,lastname,dob,gender,ssn,class,section,classroom,specialization)
+    VALUES ('$firstname','$lastname','$newdate','$gender','$ssn','$class','$section','$classroom','$specialization')";
+    $sql1 = "SELECT ssn FROM students WHERE ssn = '$ssn'";
     $result = $db->query($sql1);
-    $db->query($sql);
-    if($result->num_rows==$result+1){
+
+    if ($result->num_rows>0) {
+        $error = 'Student already insert';
+    } else {
+        $db->query($sql);
         header('location:welcome.php');
         exit();
-    }else{
-        
     }
-
 }
 ?>
-<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Insert students</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
-    <script rel="stylesheet" href="css/style.css"></script>
+
+    <title>Login Page</title>
+    <link rel="stylesheet" href="../css/data.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+
 </head>
 
 <body>
-    <form method="post" action="" style="text-align: center; padding-top:100px">
-        <br>Name:
-        <br>
-        <input type="text" name="namee">
-        <br> Surname:<br>
-        <input type="text" name="surname">
-        <br> Date of Birth:<br>
-        <input type="date" name="dob">
-        <br> Class:<br>
-        <input type="value" name="class">
-        <br> Classroom:<br>
-        <input type="text" name="classroom">
-        <br> Subject:<br>
-        <input type="text" name="subject">
-        <br><br>
-        <input type="submit" name="save" value="Submit">
-    </form>
+
+    <div class="form" style="padding-top: 80px;">
+        <div class="a-container">
+            <div class="b-container"><b>Login</b></div>
+
+            <div style="margin:30px">
+
+                <form action="" method="post">
+                    <label>FirstName:</label><input type="text" maxlength="20" name="firstname" class="box" /><br /><br />
+                    <label>LastName:</label><input type="text" name="lastname" class="box" /><br /><br />
+                    <label>DateOfBirth:</label><input type="date" name="dob" class="box"  /><br /><br />
+                    <label>Gender:</label><input type="text" name="gender" maxlength="1" class="box"  /><br /><br />
+                    <label>SSN:</label><input type="text" name="ssn" maxlength="20" class="box"  /><br /><br />
+                    <label>Class:</label><input type="text" name="class" maxlength="1" class="box"  /><br /><br />
+                    <label>Section:</label><input type="text" name="section" class="box"  /><br /><br />
+                    <label>Classroom:</label><input type="text" maxlength="10" name="classroom" class="box"  /><br /><br />
+                    <label>Specialization:</label><input type="text" maxlength="30" name="specialization" class="box"  /><br /><br />
+                    <input type="submit" value="Insert" /><br />
+                </form>
+
+                <div class="error">
+                    <?php echo $error; ?>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </body>
 
 </html>
