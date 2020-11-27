5faaa498516016f74637bcdeb34f1f0c87a6615a
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-27 10:08:57 +0100

    Create session.php
    Aggiunto un ulteriore sistema di protezione (SESSION)

---
+++ admin/session.php
@@ -0,0 +1,16 @@
+<?php
+include_once('dbconfig.php');
+
+$user_check = $_SESSION['login_user'];
+
+$ses_sql = $db->query("SELECT username FROM users WHERE username = '$user_check'");
+
+$row = $ses_sql->fetch_assoc();
+
+$login_session = $row['username'];
+
+if(!isset($_SESSION['login_user'])){
+    header('location:../index.html');
+    exit();
+}
+?>
