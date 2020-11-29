42a3ae11cd86dea07e5b93ce92ea95dcb2fd5a59
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-29 16:34:16 +0100

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+   $servername = "localhost";
+   $username = "root";
+   $password = "root";
+   $dbname = "school";
+   $db = new mysqli($servername, $username, $password, $dbname);
+   if ($db->connect_error) {
+     die("Connection failed: " . $db->connect_error);
+   }
+?>
