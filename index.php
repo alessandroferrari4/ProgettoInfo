eb50d65cbdfd3c8dec6029d0aeec6e34782944d3
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2020-11-30 21:26:00 +0100

    Update index.php
    Tolte le estensioni

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ include_once('dbconfig.php');
             <ul class="navbar-nav ml-auto">
 
                 <li class="nav-item">
-                    <a class="nav-link" href="public/login.php">Login</a>
+                    <a class="nav-link" href="public/login">Login</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="public/chart.php">Chart</a>
+                    <a class="nav-link" href="public/chart">Chart</a>
                 </li>
 
             </ul>
