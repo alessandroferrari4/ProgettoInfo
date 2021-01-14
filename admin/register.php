071da950c631fb4a497aa6b45f7aaac081eeb537
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2021-01-15 00:10:17 +0100

    Update register.php
    Sistema sql injection e agiunto il template

--- admin/register.php
+++ admin/register.php
@@ -2,16 +2,15 @@
 session_start();
 include_once('session.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $myusername = $_POST['username'];
     $mypassword = $_POST['password'];
-
     $hashPassword = password_hash($mypassword, PASSWORD_DEFAULT);
-
     $stmt = $db->prepare("SELECT username FROM users WHERE username=?");
     $stmt->bind_param("s", $myusername);
-    $result = $stmt->execute();
-    if ($result) {
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $numrow = $result->num_rows;
+    if ($numrow > 0) {
         $error = "Username già occupato";
         $stmt->close();
     } else {
@@ -28,35 +27,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Sign In</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" href="../css/styleS.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
-
-<body>
-
-    <div class="form">
-        <div class="a-container">
-            <div class="b-container"><b>Sign Up</b></div>
-
-            <div style="margin:30px">
-
-                <form action="" method="post">
-                    <label>UserName:</label><input type="text" name="username" class="box" required="" /><br /><br />
-                    <label>Password:</label><input type="password" name="password" class="box" required="" /><br /><br />
-                    <input type="submit" value="Sign Up" /><br />
-                </form>
-
-                <div class="error">
-                    <?php echo $error; ?>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</body>
+<?php
+include_once('../template/template.php');
+?>
 
 </html>
