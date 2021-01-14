053fe91cc68fb2fb5021fbc5e949966935b9262c
Alessandro <73987724+alessandroferrari4@users.noreply.github.com>
2021-01-14 23:52:57 +0100

    Update login.php
    Sistemata sql injection e aggiunto il template

--- public/login.php
+++ public/login.php
@@ -2,25 +2,25 @@
 session_start();
 include_once('../dbconfig.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $myusername = $_POST['username'];
     $mypassword = $_POST['password'];
-
     $stmt = $db->prepare("SELECT * FROM users WHERE username=?");
     $stmt->bind_param("s", $myusername);
-    $result = $stmt->execute();
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $numrow = $result->num_rows;
     $stmt->close();
-
-    if ($result) {
+    if ($numrow > 0) {
         $result = $db->query("SELECT passcode FROM users WHERE username='$myusername'");
         $row = $result->fetch_assoc();
         if (password_verify($mypassword, $row['passcode'])) {
             $_SESSION['login_user'] = $myusername;
             header("location:../admin/welcome");
             exit();
-        } else {
-            $error = "Wrong username or password!";
-        }
+        } else
+            $error = 'Wrong password!';
+    } else {
+        $error = "Wrong username!";
     }
 }
 
@@ -35,30 +35,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </head>
 
-<body>
-
-    <div class="form">
-        <div class="a-container">
-            <div class="b-container"><b>Login</b></div>
-
-            <div style="margin:30px">
-
-                <form action="" method="post">
-                    <label>UserName:</label><input type="text" name="username" class="box" /><br /><br />
-                    <label>Password:</label><input type="password" name="password" class="box" /><br /><br />
-                    <input type="submit" value=" Submit " /><br />
-                </form>
-
-                <div class="error">
-                    <?php echo $error; ?>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</body>
+<?php
+include_once('../template/template.php');
+?>
 
 </html>
